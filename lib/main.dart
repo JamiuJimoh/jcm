1d66d3ff6a97c30d2d39bdd0b8e5d4d549158f28
Janiu <hyperjay55@gmail.com>
2021-07-11 21:42:56 +0100

    changed the system ui overlay style

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'app/landing_page.dart';
 import 'app_theme.dart';
 import 'services/auth.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF512DA8),
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Provider<AuthBase>(
-      create:(_)=>Auth(),
+      create: (_) => Auth(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: AppTheme.lightMode,
