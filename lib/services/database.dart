b1fa145f11111336b2c3f93dec4fe8775f2dc168
Janiu <hyperjay55@gmail.com>
2021-07-12 21:03:41 +0100

    added firestore add stored userType in a collection

---
+++ lib/services/database.dart
@@ -0,0 +1,65 @@
+// import 'api_path.dart';
+// import 'firestore_service.dart';
+
+import 'package:jamiu_class_manager/app/home/models/user.dart';
+
+import 'api_path.dart';
+import 'firestore_service.dart';
+
+abstract class Database {
+  Future<void> setUserType(UserModel userModel);
+  Stream<List<UserModel>> usersStream();
+  // Stream<UserModel> userStream();
+//   Stream<List<Meal>> mealsStream();
+//   Future<void> deleteMeal(Meal meal);
+//   Stream<List<FavoriteMeal>> favoriteMealsStream();
+//   Future<void> setFavoriteMeal(FavoriteMeal favoriteMeal);
+//   // Future<void> deleteFavoriteMeal(Meal meal);
+}
+
+String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
+
+class FireStoreDatabase implements Database {
+  final String uid;
+
+  FireStoreDatabase({required this.uid});
+
+  final _service = FirestoreService.instance;
+
+  @override
+  Stream<List<UserModel>> usersStream() => _service.collectionStream<UserModel>(
+        path: APIPath.userTypes(uid: uid),
+        builder: (data, documentId) => UserModel.fromMap(data),
+      );
+
+  @override
+  Future<void> setUserType(UserModel userModel) {
+    return _service.setData(
+      path: APIPath.userType(uid: uid, userTypeId: uid),
+      data: userModel.toMap(),
+    );
+  }
+
+//   @override
+//   Future<void> deleteMeal(Meal meal) => _service.deleteData(
+//         path: APIPath.meal(mealId: meal.mealId),
+//       );
+
+//   @override
+//   Stream<List<FavoriteMeal>> favoriteMealsStream() =>
+//       _service.collectionStream<FavoriteMeal>(
+//         path: APIPath.favoriteMeals(uid: uid),
+//         builder: (data, documentId) => FavoriteMeal.fromMap(data, documentId),
+//       );
+
+//   @override
+//   Future<void> setFavoriteMeal(FavoriteMeal favoriteMeal) => _service.setData(
+//         path: APIPath.favoriteMeal(uid: uid, mealId: favoriteMeal.mealId),
+//         data: favoriteMeal.toMap(),
+//       );
+
+//   // @override
+//   // Future<void> deleteFavoriteMeal(Meal meal) => _service.deleteData(
+//   //       path: APIPath.favoriteMeal(uid: uid, mealId: meal.mealId),
+//   //     );
+}
